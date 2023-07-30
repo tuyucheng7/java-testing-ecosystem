401eb0bc511bec4c07e3d3d2a3eaaf19b9418590
taketoday <tuyucheng2000@163.com>
2023-07-30 13:47:29 +0800

    🚚 rename class

--- greenmail/src/main/java/cn/tuyucheng/taketoday/GreenmailApplication.java
+++ greenmail/src/main/java/cn/tuyucheng/taketoday/GreenmailApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class GreenmailApplication {
 
    public static void main(String[] args) {
-      SpringApplication.run(Application.class, args);
+      SpringApplication.run(GreenmailApplication.class, args);
    }
 }
--- load-testing-comparison/src/main/java/cn/tuyucheng/taketoday/loadtesting/LoadTestingApplication.java
+++ load-testing-comparison/src/main/java/cn/tuyucheng/taketoday/loadtesting/LoadTestingApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class LoadTestingApplication {
 
    public static void main(String[] args) {
-      SpringApplication.run(Application.class, args);
+      SpringApplication.run(LoadTestingApplication.class, args);
    }
 }
